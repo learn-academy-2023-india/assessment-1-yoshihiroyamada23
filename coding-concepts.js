6508c63c92a0b52fa3ccfe2764a16842c9110889
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2023-11-25 21:15:19 -0800

    finished all assessments

--- coding-concepts.js
+++ coding-concepts.js
@@ -16,40 +16,41 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2023"
-// console.log(cohort.length)
+//console.log(cohort.length)
 
-// a) Your answer:
+// a) Your answer: 10
 // b) Verify and explain:
-
+// looking at the syllabus under arrays.md, it shows how .length means how many indexs are from LEARN 2023
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
-// console.log(greeting[4])
+//console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: o
+// b) Verify and explain: My answer is correct because looking at my notes, the [4] means we're looking at the letter o from hello
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
-// console.log(languages[index])
+ //console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ruby
+// b) Verify and explain: the const index = 1 is telling me that we are looking for the idex of 1 and it is the same thing as console.log(languages[1]). 
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "saturday" = "Saturday" and "sunday"= "Sunday"
+// b) Verify and explain: The .toUppserCase() means that we are putting whatever is in [] to uppercase and I verified this through my notes and from the syllabus arrays.md. some reason it gave me an error.
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+//  console.log(typeof dataTypes.length)
 
-// a) Your answer:
+// a) Your answer: i do not know the answer
 // b) Verify and explain:
+// the typeof operator is used to get the data type of aa variable or expression. the .lngth is a property that represents the number of elements in an array.
